b713d012d0b2a64fdc80736f2d63175abc4df4ce
MohaProgra <58303651+MohaProgra@users.noreply.github.com>
2020-03-24 15:43:06 +0100

    Initial commit

---
+++ app/src/main/java/com/example/holamundo4/Ventana2.java
@@ -0,0 +1,18 @@
+package com.example.holamundo4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Ventana2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana2);
+    }
+
+    }
+
